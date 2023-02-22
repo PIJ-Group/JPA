9888a6d174d82f874f1bbe9e4e31006b796c8e23
Jorge <jorgebuendia@edix.com>
2023-02-22 21:44:32 +0100

    Incluido supuesto Jorge

---
+++ src/main/java/modelo/entidad/jorge/Taller.java
@@ -0,0 +1,54 @@
+package modelo.entidad.jorge;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "talleres")
+public class Taller {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;	
+	private String nombre;
+	@OneToOne(mappedBy = "direccion", cascade = CascadeType.ALL)
+	private Direccion direccion;
+	
+	public Taller() {
+		super();
+	}
+
+	public Taller(Integer id, String nombre, Direccion direccion) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.direccion = direccion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Direccion getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+
+	@Override
+	public String toString() {
+		return "Taller [id=" + id + ", nombre=" + nombre + ", direccion=" + direccion + "]";
+	}
+	
+}
